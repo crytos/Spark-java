91b34b6d82b5b969d0870b86c7f9b1728e71469c
cloudera <cloudera@quickstart.cloudera>
2016-06-25 03:19:25 -0700

    update launch script

--- run-spark.sh
+++ run-spark.sh
@@ -9,11 +9,6 @@ OPTS+="--files /etc/hive/conf/hive-site.xml "
 JARS="--jars /usr/jars/datanucleus-core-3.2.10.jar,"
 JARS+="/usr/jars/datanucleus-api-jdo-3.2.6.jar,/usr/jars/datanucleus-rdbms-3.2.9.jar"
 
-#NB_RECORDS=$1
-#NB_ARCHIVES=$2
-
 echo $SPARK_SUBMIT $OPTS $JARS target/POC13-1.0-SNAPSHOT-jar-with-dependencies.jar
-#$NB_RECORDS $NB_ARCHIVES
 
 exec $SPARK_SUBMIT $OPTS $JARS target/POC13-1.0-SNAPSHOT-jar-with-dependencies.jar
-#$NB_RECORDS $NB_ARCHIVES
