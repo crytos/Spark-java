4b20dec96633c6e1c0b0408a1f85e58c66edcd1d
cloudera <cloudera@quickstart.cloudera>
2016-07-02 17:03:29 -0700

    update utils

--- src/main/java/fr/edf/dco/dn/utils/Constants.java
+++ src/main/java/fr/edf/dco/dn/utils/Constants.java
@@ -5,6 +5,8 @@ package fr.edf.dco.dn.utils;
  */
 public interface Constants {
 
+	String DB_PROPERTIES_FILE_NAME = "/properties/database.properties";
+	
     String DB_NAME = "db.name";
     String CUSTOMERS_ORDERS = "customers_orders";
     String HDFS_OUTPUT_DIRECTORY = "output.directory";
