8c011b7fb04ed6fa5e2e95a27db10ab90548c8d4
cloudera <cloudera@quickstart.cloudera>
2016-11-27 06:55:54 -0800

    update spark-submit script

--- spark/spark-submit.sh
+++ spark/spark-submit.sh
@@ -2,13 +2,13 @@
 
 SPARK_SUBMIT=/usr/bin/spark-submit
 
-OPTS="--name APP-SPARK --class fr.edf.dco.dn.batch.$1 --master yarn --deploy-mode cluster "
+OPTS="--name APP-SPARK --class crytos.hadoop.batch.$1 --master yarn --deploy-mode cluster "
 
 OPTS+="--files /etc/hive/conf/hive-site.xml "
 
-JARS="--jars /usr/jars/datanucleus-core-3.2.10.jar,"
-JARS+="/usr/jars/datanucleus-api-jdo-3.2.6.jar,/usr/jars/datanucleus-rdbms-3.2.9.jar"
+#JARS="--jars /usr/jars/datanucleus-core-3.2.10.jar,"
+#JARS+="/usr/jars/datanucleus-api-jdo-3.2.6.jar,/usr/jars/datanucleus-rdbms-3.2.9.jar"
 
-echo $SPARK_SUBMIT $OPTS $JARS target/POC13-1.0-SNAPSHOT-jar-with-dependencies.jar
+echo $SPARK_SUBMIT $OPTS  target/spark.practice-0.1-jar-with-dependencies.jar
 
-exec $SPARK_SUBMIT $OPTS $JARS target/POC13-1.0-SNAPSHOT-jar-with-dependencies.jar
+exec $SPARK_SUBMIT $OPTS  target/spark.practice-0.1-jar-with-dependencies.jar
